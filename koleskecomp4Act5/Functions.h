1071d6162fbac6b657357a04726e9a5581d3c365
Milo Koleske <144748100+miloKoleske@users.noreply.github.com>
2025-02-26 15:15:38 -0700

    uploaded as of end of class session 13
    2/26/25

---
+++ koleskecomp4Act5/Functions.h
@@ -0,0 +1,35 @@
+// Milo Koleske
+// mkoleske@cnm.edu
+// koleskecomp4Act5
+
+#ifndef FUNCTION_H
+#define FUNCTION_H
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+void WriteHeader();
+
+int Menu();
+
+void InchesToFeetAndInches(int totalInches);
+
+void FeetAndInchesToDecimalFeet(int feet, int inches);
+
+void FeetAndInchesToInches(int feet, int inches);
+
+void Goodbye();
+
+
+
+
+
+
+
+
+
+
+
+#endif // goes at the end of the file 
