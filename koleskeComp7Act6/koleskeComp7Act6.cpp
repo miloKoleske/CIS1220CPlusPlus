40caba048b486863add845a381879663698275e4
Milo Koleske <144748100+miloKoleske@users.noreply.github.com>
2025-04-07 15:14:28 -0600

    koleskecomp7act6
    updated as of 4/7/25

---
+++ koleskeComp7Act6/koleskeComp7Act6.cpp
@@ -0,0 +1,18 @@
+// Milo Koleske
+// 
+#include <iostream>
+
+int main()
+{
+    std::string hr = "________________________________________________________________\n";
+    std::cout << "Programmer name: Milo Koleske\n";
+    std::cout << "Program title: Favorite Movies\n";
+    std::cout << "Program objective: Record favorite movies and save data to another file.\n";
+    std::cout << hr;
+
+    std::cout << "\nThis is a program to record a list of my favorite movies.\n";
+
+
+
+    return 0;
+}
